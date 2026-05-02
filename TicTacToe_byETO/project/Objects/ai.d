b4a806d446f17a55f6210c449a8518a45d258dd6
ETO <130556647+ETO-QSH@users.noreply.github.com>
2026-05-02 17:48:24 +0800

    Delete TicTacToe_byETO/project/Objects directory

--- TicTacToe_byETO/project/Objects/ai.d
+++
@@ -1,4 +0,0 @@
-.\objects\ai.o: ..\App\ai.c
-.\objects\ai.o: ..\App\ai.h
-.\objects\ai.o: D:\Keil\MDK\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\ai.o: D:\Keil\MDK\ARM\ARMCC\Bin\..\include\string.h
--- TicTacToe_byETO/project/Objects/startup_gd32h7xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_gd32h7xx.o: ..\CMSIS\GD\GD32H7xx\Source\ARM\startup_gd32h7xx.s
