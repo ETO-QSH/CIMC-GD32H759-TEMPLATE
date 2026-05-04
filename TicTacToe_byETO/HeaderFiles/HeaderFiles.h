3be6961a1d39975859173663745e393748c8d983
ETO-QSH <2373204754@qq.com>
2026-05-04 12:02:57 +0800

    nothing changed

--- TicTacToe_byETO/HeaderFiles/HeaderFiles.h
+++ TicTacToe_byETO/HeaderFiles/HeaderFiles.h
@@ -1,15 +1,5 @@
-/************************************************************
- * 版权：2025CIMC Copyright。 
- * 文件：Headerfiles.h
- * 作者: Jialei Zhao
- * 平台: 2025 CIMC IHD V04
- * 版本: Jialei Zhao     2026/2/5     V0.01    original
-************************************************************/
-
 #ifndef __HEADERFILES_H
 #define __HEADERFILES_H
-
-/************************* 头文件 *************************/
 
 #include "gd32h7xx.h"
 #include "gd32h7xx_libopt.h"
@@ -17,9 +7,6 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "string.h"
-#include "Function.h"     // 执行函数
+#include "Function.h"
 
 #endif
-
-/****************************End*****************************/
-
